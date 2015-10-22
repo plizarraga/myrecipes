de8255715314374c2e0d75bb7abd1225376a692b
plizarraga <plizarraga86@gmail.com>
2015-10-22 15:19:58 -0700

    Se agregaron las validaciones al modelo de Chef y los test correspondientes.

---
+++ test/models/chef_test.rb
@@ -0,0 +1,59 @@
+require 'test_helper'
+
+class ChefTest < ActiveSupport::TestCase
+	def setup
+		@chef = Chef.new(name: "Eddie", email: "eddie@example.com")
+	end
+
+	test 'chef debe ser valido' do
+		assert @chef.valid?
+	end
+
+	test 'name debe estar presente' do
+		@chef.name = " "
+		assert_not @chef.valid?
+	end
+
+	test 'name no debe ser muy largo' do
+		@chef.name = "a"*41
+		assert_not @chef.valid?
+	end
+
+	test 'name no debe ser muy corto' do
+		@chef.name = "aa"
+		assert_not @chef.valid?
+	end
+
+	test 'email debe estar presente' do
+		@chef.email = " "
+		assert_not @chef.valid?
+	end
+
+	test 'email no debe ser muy largo' do
+		@chef.email = "a"*101+"@example.com"
+		assert_not @chef.valid?
+	end
+
+	test 'email debe ser unica' do
+		dup_chef = @chef.dup 
+		dup_chef.email = @chef.email.upcase
+		@chef.save
+		assert_not dup_chef.valid?
+	end
+
+	test "email validation should accept valid addresses" do
+		valid_addresses = %w[user@eee.com R_TDD-DS@eee.hello.org user@example.com first.last@eem.au laura+joe@monk.cm]
+		valid_addresses.each do |va|
+			@chef.email = va
+			assert @chef.valid?, '#{va.inspect} should be valid'
+		end
+	end
+
+	test "email validation should reject invalid addresses" do
+		invalid_addresses = %w[user@example,com user_at_eee.org user.name@example. eee@i_am_.com foo@ee+aar.com]
+		invalid_addresses.each do |ia|
+			@chef.email = ia
+			assert_not @chef.valid?, '#{ia.inspect} should be invalid'
+		end
+	end
+end
