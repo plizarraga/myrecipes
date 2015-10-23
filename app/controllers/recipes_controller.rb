3a735613b4d8988466673f98c70b8a13c95038db
plizarraga <plizarraga86@gmail.com>
2015-10-22 23:40:32 -0700

    Se agregaron las rutas de recipes y el controlador y la vista de index para las recetas.

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,23 @@
+class RecipesController < ApplicationController
+	def index
+		@recipes = Recipe.all
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def delete
+	end
+end
