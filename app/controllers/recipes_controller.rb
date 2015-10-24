fbc3790115105d0f56c27fedc7565bc32073e9ab
plizarraga <plizarraga86@gmail.com>
2015-10-24 14:17:44 -0700

    Se agrego la forma de nueva recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,17 +6,18 @@ class RecipesController < ApplicationController
 	def show
 		@recipe = Recipe.find(params[:id])
 	end
+	
+	def new
+		@recipe = Recipe.new
+	end
 
+	def create
+	end
+	
 	def edit
 	end
 
 	def update
-	end
-
-	def new
-	end
-
-	def create
 	end
 
 	def delete
